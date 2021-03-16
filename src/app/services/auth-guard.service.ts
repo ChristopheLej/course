e96e57f19b5a8d920264d045c48b92ca0bf0597f
CMJC2158 <CMJC2158@WX-OR6218438>
2021-03-16 14:19:29 +0100

    Add redirect when clicking courses menu

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import { ApplicationState } from '@storeConfig';
 import { isLoggedIn } from '@store/selectors/auth.selector';
+import { map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private store: Store<ApplicationState>) {}
+  constructor(private store: Store<ApplicationState>, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    return this.store.pipe(select(isLoggedIn));
+    return this.store.select(isLoggedIn).pipe(
+      map(login => {
+        if (!login) {
+          return this.router.parseUrl('/login');
+        }
+        return login;
+      })
+    );
   }
 }
